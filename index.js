3d756ca5ed57b590f96432e6e16e2f9544b71c1f
jim <zyi103@syr.edu>
2019-04-21 18:03:21 -0400

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public2'))
+app.use(express.static('/var/www/html/'))
 app.listen(3000, () => console.log('Server running on port 3000'))

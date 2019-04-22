4a8b8383be58a9914f8a32a841f69a2fab21cf35
jim <zyi103@syr.edu>
2019-04-22 00:54:11 -0400

    got that data baby

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server running on port 3000'))
 
-app.use('/', express.static('public'))
-app.get('/backend', (req, res) => res.send('Hello World!'))
-
-app.listen(80, () => console.log('Example app listening on port 80!'))

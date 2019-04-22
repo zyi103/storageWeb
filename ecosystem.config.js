b7c5495e62f4853d136b6832aa359f0b5a75d12f
jim <zyi103@syr.edu>
2019-04-22 01:13:37 -0400

    firebase

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-191-231-230.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/storage.pem',
+      key: '~/.ssh/jim1-storagekey.pem',
       ref: 'origin/master',
       repo: 'git@github.com:zyi103/storage.git',
       path: '/home/ubuntu/storage',
